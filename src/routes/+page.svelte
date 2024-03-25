0473ef207a67f9fb78e33a2a21930d8be2770487
stijnvanhoey <stijnvanhoey@gmail.com>
2024-03-25 22:41:14 +0100

    Add dialog for extra info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,9 @@
   import { onMount } from 'svelte';
   import { browser } from '$app/environment';
 	import ol_control_Legend from "ol-ext/control/Legend";
+  import { Drawer } from "vaul-svelte";
   //import monstersData from '$lib/data_watermonsters.geojson?raw';
+
 
   let target;
   let map;
@@ -520,7 +522,6 @@
     }
   });
 
-
   // Control Select
   let select = new Select({
     layers: [dataLayer],
@@ -605,15 +606,74 @@
   }});
 </script>
 
-<div class="ol-container" bind:this={target} />
+
+<div class="ol-container" bind:this={target}>
+  <Drawer.Root>
+    <Drawer.Trigger class="absolute top-1 right-1 rounded-md bg-white px-4 py-2.5 text-sm font-semibold text-gray-900
+    shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 m-5 z-10">Meer info</Drawer.Trigger>
+    <Drawer.Portal>
+      <Drawer.Overlay class="fixed inset-0 bg-black/20 opacity-65" />
+      <Drawer.Content
+        class="bg-zinc-100 flex flex-col rounded-t-[10px] mt-24 fixed bottom-0 left-0 right-0 h-[50%]"
+      >
+        <div class="p-4 bg-white rounded-t-[10px] flex-1">
+          <div class="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8" />
+          <div class="max-w-md mx-auto">
+            <Drawer.Title class="font-medium mb-4">
+              Watermonsters
+            </Drawer.Title>
+            <p class="text-zinc-600 mb-2">
+              Achtergrond info over campagne...
+            </p>
+            <p class="text-zinc-600 mb-2 text-lg font-medium">Meer info over de icoontjes...</p>
+            <p class="text-zinc-600 mb-2 text-lg">Nabijheid overstort...</p>
+          </div>
+        </div>
+        <div class="p-4 bg-zinc-100 border-t border-zinc-200 mt-auto">
+          <div class="flex gap-6 justify-end max-w-md mx-auto">
+            <a
+              class="text-xs text-zinc-600 flex items-center gap-0.25"
+              href="https://www.waterlandvzw.be/"
+              target="_blank"
+            >
+              waterlandvzw
+              <svg fill="currentcolor"
+              stroke="currentColor"
+              height="16" width="16"
+              version="1.1"
+              viewBox="0 0 512 512"
+              class="w-3 h-3 ml-1">
+                <g>
+                  <g>
+                    <path d="M256,0C114.62,0,0,114.62,0,256s114.62,256,256,256s256-114.62,256-256S397.38,0,256,0z M172.211,41.609
+                      c-24.934,27.119-44.68,66.125-56.755,111.992H49.749C75.179,102.741,118.869,62.524,172.211,41.609z M25.6,256
+                      c0-26.999,5.077-52.727,13.662-76.8h70.494c-4.608,24.294-7.356,49.963-7.356,76.8s2.748,52.506,7.347,76.8H39.262
+                      C30.677,308.727,25.6,283,25.6,256z M49.749,358.4h65.707c12.083,45.867,31.821,84.872,56.755,111.991
+                      C118.869,449.476,75.179,409.259,49.749,358.4z M243.2,485.188c-43.81-8.252-81.877-58.24-101.359-126.788H243.2V485.188z
+                        M243.2,332.8H135.74c-4.924-24.166-7.74-49.997-7.74-76.8s2.816-52.634,7.74-76.8H243.2V332.8z M243.2,153.6H141.841
+                      C161.323,85.052,199.39,35.063,243.2,26.812V153.6z M462.251,153.6h-65.707c-12.083-45.867-31.821-84.873-56.755-111.992
+                      C393.131,62.524,436.821,102.741,462.251,153.6z M268.8,26.812c43.81,8.252,81.877,58.24,101.359,126.788H268.8V26.812z
+                        M268.8,179.2h107.46c4.924,24.166,7.74,49.997,7.74,76.8s-2.816,52.634-7.74,76.8H268.8V179.2z M268.8,485.188V358.4h101.359
+                      C350.677,426.948,312.61,476.937,268.8,485.188z M339.789,470.391c24.934-27.127,44.672-66.125,56.755-111.991h65.707
+                      C436.821,409.259,393.131,449.476,339.789,470.391z M402.244,332.8c4.608-24.294,7.356-49.963,7.356-76.8
+                      s-2.748-52.506-7.347-76.8h70.494c8.576,24.073,13.653,49.801,13.653,76.8c0,27-5.077,52.727-13.662,76.8H402.244z"/>
+                  </g>
+           </g>
+           </svg>
+            </a>
+          </div>
+        </div>
+      </Drawer.Content>
+    </Drawer.Portal>
+  </Drawer.Root>
+</div>
 
 <style>
   :global(html, body, #svelte),
-  div {
+  .ol-container {
     width: 100%;
     height: 100%;
     margin: 0;
     padding: 0;
   }
-
 </style>
